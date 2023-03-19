17c939d910a0cf23a8447a0f1bf5c772843f277c
jyotibhagat <jyotibhagat114@gmail.com>
2023-03-19 18:06:00 +0000

    final python deploy gke using jenmkisn

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'python app'
+    return ' we did it finally,python app'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , port = 8080)
