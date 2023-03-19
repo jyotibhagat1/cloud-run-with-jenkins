45bd9151a41905d538bf482ab5602c87577c946a
jyotibhagat <jyotibhagat114@gmail.com>
2023-03-19 16:45:36 +0000

    updated code added second line

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'welcome to my first python app'
+    return 'welcome to my second python app'
 
-if __name__ == '__main__':
+ if __name__ == '__main__':
    app.run(host='0.0.0.0' , port = 8080)
