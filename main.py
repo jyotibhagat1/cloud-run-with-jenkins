8e674902318c4331060ebe42f237342ea7882177
jyotibhagat <jyotibhagat114@gmail.com>
2023-03-19 17:49:07 +0000

    added jyoti

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello , from  python app'
+    return 'i am jyoti , from  python app'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , port = 8080)
